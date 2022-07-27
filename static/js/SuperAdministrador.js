3e5df53bc45afdb141fc97bf15e780130d324a1d
Santiago Ramon <santiramon967gmail.com>
2022-07-26 22:42:20 -0500

    HTML, js, css inicio de sesion, registro y SuperAdministrador

---
+++ static/js/SuperAdministrador.js
@@ -0,0 +1,52 @@
+function abrirFormulario1(){
+    var frm1 = document.form1;
+    var frm2= document.form2;
+    var frm3= document.form3;
+
+    if(frm1.style.display== "block"){
+        frm1.style.display="none";
+        frm2.style.display="none";
+        frm3.style.display="none";
+
+
+    
+    }else if(frm1.style.display== "none"){
+        frm1.style.display="block";
+        frm2.style.display="none";
+        frm3.style.display="none";
+    }  
+}
+
+function abrirFormulario2(){
+    var frm1= document.form1;
+    var frm2= document.form2;
+    var frm3= document.form3;
+
+    if(frm2.style.display=="block"){
+        frm2.style.display="none";
+        frm1.style.display="none";
+        frm3.style.display="none";
+    
+    }else if(frm2.style.display== "none"){
+        frm2.style.display="block";
+        frm1.style.display="none";
+        frm3.style.display="none";
+    } 
+}
+
+function abrirFormulario3(){
+    var frm1= document.form1;
+    var frm2= document.form2;
+    var frm3= document.form3;
+
+    if(frm3.style.display=="block"){
+        frm3.style.display="none";
+        frm1.style.display="none";
+        frm2.style.display="none";
+    
+    }else if(frm3.style.display== "none"){
+        frm3.style.display="block";
+        frm2.style.display="none";
+        frm1.style.display="none";
+    } 
+}
